115e79afb05e68392d03272df1086cb66bdfc239
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-10-28 21:57:55 +0000

    Refactor tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-
-  it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
+  it { is_expected.to respond_to :release_bike }
 end
