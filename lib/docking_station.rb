76422b7496742bfad73b674a40491ad766336f4e
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-10-29 21:43:54 +0000

    Test bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,8 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,4 +12,10 @@ describe DockingStation do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
+
+  it 'sees stored bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
