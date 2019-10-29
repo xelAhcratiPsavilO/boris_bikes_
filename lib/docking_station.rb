5236d33539b062935ce1d434e72c5fbcd6a9ce93
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-10-29 22:21:31 +0000

    Refactor following single responsability principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -11,12 +11,23 @@ class DockingStation
   attr_reader :bikes
 
   def release_bike
-    fail 'no bikes available' if @bikes.empty?
+    fail 'no bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'station full' if @bikes.count >= CAPACITY
+    fail 'station full' if full?
     @bikes << bike
   end
+
+  private
+
+  def empty?
+    @bikes.empty?
+  end
+
+  def full?
+    @bikes.count >= CAPACITY
+  end
+
 end
