952a552e85839a528e0a45342925bf2a0ab73453
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-11-03 22:01:22 +0000

    Make Van class responds to method

--- lib/van.rb
+++ lib/van.rb
@@ -1,2 +1,6 @@
 class Van
+
+  def take_broken_bikes_from(docking_station)
+
+  end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,4 +2,6 @@ require 'van'
 
 describe Van do
 
+  it { is_expected.to respond_to(:take_broken_bikes_from).with(1).argument }
+
 end
